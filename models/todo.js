c8699d852d68c487019e0398fe227d39198fc5a3
Kevin Hubbard <dgkallday98@gmail.com>
2017-05-01 20:34:14 -0400

    initial commit

---
+++ models/todo.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  var Todo = sequelize.define("Todo", {
+    text: {
+
+     type: DataTypes.STRING,
+     validate:{
+     	len:[1,140],
+     	
+     }
+ 	},
+    complete: DataTypes.BOOLEAN
+  });
+  return Todo;
+};
